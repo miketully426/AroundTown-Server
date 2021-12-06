7ccbcc2b37e37750fdec87c55cbdad33a67f166c
Jessica Mertz <mertzjl91@gmail.com>
2021-12-06 17:18:13 -0600

    initial commit

---
+++ src/main/java/com/launchcode/AroundTownServer/AroundTownServerApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.AroundTownServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroundTownServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AroundTownServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/launchcode/AroundTownServer/AroundTownServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchcode.AroundTownServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroundTownServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
