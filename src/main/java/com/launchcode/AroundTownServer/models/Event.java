bb0cdd902a3d55f72a86adb4c9ad5529beab08c4
Eydie <you@example.com>
2021-12-06 19:26:20 -0600

    Created event object #1 on story of "Create Event"

---
+++ src/main/java/com/launchcode/AroundTownServer/models/Event.java
@@ -0,0 +1,50 @@
+package com.launchcode.AroundTownServer.models;
+
+
+//import javax.validation.constraints.NotNull;
+
+public class Event {
+
+//    @NotNull(message = "Name is required to be inputted!")
+    public String name;
+
+    public String description;
+
+    public String location;
+
+
+    public Event(String name, String description, String location) {
+        this.name = name;
+        this.description = description;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
