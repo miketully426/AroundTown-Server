0602ad798a9f3a336dc3320de5d715ea9c42492b
13145 <lfallert13@yahoo.com>
2021-12-18 11:32:32 -0600

    added events feature

---
+++ src/main/java/com/launchcode/AroundTownServer/controllers/EventController.java
@@ -0,0 +1,32 @@
+package com.launchcode.AroundTownServer.controllers;
+
+import com.launchcode.AroundTownServer.data.EventRepository;
+import com.launchcode.AroundTownServer.models.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("api")
+@ResponseBody
+public class EventController {
+
+    @Autowired
+    private final EventRepository eventRepository;
+
+    public EventController (EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @GetMapping("/events")
+    public List<Event> getAllEvents() {
+        return (List<Event>) eventRepository.findAll();
+    }
+
+    @PostMapping("/events")
+    void addEvent(@RequestBody Event event) {
+        eventRepository.save(event);
+    }
+
+}
---
+++ src/main/java/com/launchcode/AroundTownServer/models/Event.java
@@ -0,0 +1,53 @@
+package com.launchcode.AroundTownServer.models;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private int eventId;
+
+    private String name;
+
+    private String description;
+
+    private String location;
+
+    private String date;
+
+    private String time;
+
+    private String entryCost;
+
+    private boolean familyFriendly;
+
+    public Event(String name, String description, String location, String date, String time, String entryCost, boolean familyFriendly) {
+        this.name = name;
+        this.description = description;
+        this.location = location;
+        this.date = date;
+        this.time = time;
+        this.entryCost = entryCost;
+        this.familyFriendly = familyFriendly;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Event)) return false;
+        Event event = (Event) o;
+        return eventId == event.eventId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(eventId);
+    }
+
+}
