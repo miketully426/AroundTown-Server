a4e4b6b3453db91bed59787a407ac61cf156a8f5
Cara Noyes <cnnnoyes@outlook.com>
2022-01-02 18:07:22 -0600

    removed unsused code in UserRepository

--- src/main/java/com/launchcode/AroundTownServer/data/UserRepository.java
+++ src/main/java/com/launchcode/AroundTownServer/data/UserRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     User findByUsername(String username);
-    User findByEmail(String email);
 }
