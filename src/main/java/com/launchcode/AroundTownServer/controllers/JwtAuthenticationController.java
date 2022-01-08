2c106a5d83aac85d511d2a856777e7f10a4cd648
Eydie <aeydie@gmail.com>
2022-01-07 23:02:29 -0600

    latest changes - goodnight

--- src/main/java/com/launchcode/AroundTownServer/controllers/JwtAuthenticationController.java
+++ src/main/java/com/launchcode/AroundTownServer/controllers/JwtAuthenticationController.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-
-
+import com.launchcode.AroundTownServer.models.User;
 
 
 @RestController
--- src/main/java/com/launchcode/AroundTownServer/models/ERole.java
+++
@@ -1,7 +0,0 @@
-//package com.launchcode.AroundTownServer.models;
-//
-//public enum ERole {
-//
-//    ROLE_USER,
-//    ROLE_ADMIN
-//}
