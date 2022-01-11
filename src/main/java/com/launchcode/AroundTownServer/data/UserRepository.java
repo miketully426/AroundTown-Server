63e500d1443309da4b79edee53274d8790d03544
lfallert <83887255+lfallert@users.noreply.github.com>
2022-01-11 17:21:52 -0600

    Update UserRepository.java
    removed unused method

--- src/main/java/com/launchcode/AroundTownServer/data/UserRepository.java
+++ src/main/java/com/launchcode/AroundTownServer/data/UserRepository.java
@@ -9,5 +9,4 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByUsername(String username);
-    Optional<User> deleteByUsername(String username);
 }
